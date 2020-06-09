4a36cb2880f17389402538c27a183bcf0daac23a
Travis Shen <TravyLockett@gmail.com>
2020-06-08 23:02:35 -0700

    change port #

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ application.get("/", (req, res) => {
 
 application.use("/user", userController);
 
-application.listen("3000", () => {
+application.listen("4000", () => {
   console.log("server started");
 });
